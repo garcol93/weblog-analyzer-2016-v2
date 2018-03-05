30695df1664440474276867036e81f6dbcb8f93b
Your Name <you@example.com>
2018-03-05 17:45:51 +0100

    la clase Acceso ahora recibe un string con fecha y hora

--- Acceso.java
+++ Acceso.java
@@ -1,3 +1,10 @@
+/**
+ * Modela objetos Acceso. 
+ * Los objetos Acceso controlan todos los datos relativos a fecha y hora de una conexion al servidor.
+ * 
+ * @author garcol93
+ * @version 2018/03/05
+ */
 public class Acceso
 {
     private int ano;
@@ -5,36 +12,62 @@ public class Acceso
     private int dia;
     private int hora;
     private int minutos;
-    
-    public Acceso(int ano, int mes, int dia, int hora, int minutos)
+
+    /**
+     * Construye objetos Acceso.
+     * @param String con fecha y hora completa
+     */
+    public Acceso(String texto)
     {
-        this.ano = ano;
-        this.mes = mes;
-        this.dia = dia;
-        this.hora = hora;
-        this.minutos = minutos;
+        String[] strings = texto.split(" ");                
+        this.ano = Integer.parseInt(strings[0]);
+        this.mes =Integer.parseInt(strings[1]);
+        this.dia = Integer.parseInt(strings[2]);
+        this.hora =Integer.parseInt(strings[3]);
+        this.minutos = Integer.parseInt(strings[4]);
     }
-    
+
+    /**
+     * Devuelve el ano en el que se ha producido el Acceso al servidor.
+     * @return Devuelve el ano en el que se ha producido el Acceso al servidor.
+     */
+
     public int getAno() 
     {
         return ano;
     }
-    
+
+    /**
+     * Devuelve el mes en el que se ha producido el Acceso al servidor.
+     * @return Devuelve el ano en el que se ha producido el Acceso al servidor.
+     */
     public int getMes()
     {
         return mes;
     }
-    
+
+    /**
+     * Devuelve el dia en el que se ha producido el Acceso al servidor.
+     * @return Devuelve el ano en el que se ha producido el Acceso al servidor.
+     */
     public int getDia()
     {
         return dia;
     }
-    
+
+    /**
+     * Devuelve la hora en la que se ha producido el Acceso al servidor.
+     * @return Devuelve el ano en el que se ha producido el Acceso al servidor.
+     */
     public int getHora()
     {
         return hora;
     }
-    
+
+    /**
+     * Devuelve el minuto en el que se ha producido el Acceso al servidor.
+     * @return Devuelve el ano en el que se ha producido el Acceso al servidor.
+     */
     public int getMinutos()
     {
         return minutos;
